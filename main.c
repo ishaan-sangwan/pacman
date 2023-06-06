2dcbd31e54ce435c5103970d6fe2da46c1d5e796
ishaan sangwan <ishaansangwan@gmail.com>
2023-06-06 13:20:42 +0530

    created a new file for the movement function

--- main.c
+++ main.c
@@ -1,37 +1,8 @@
 #include "render.h"
 #include <stdlib.h>
 #include <conio.h>
+#include "movement.h"
 
-void movement_pacman(int key){
-    switch(key) { // the real value
-        case 'w':
-            // code for arrow up
-            move_up();
-            
-            system("cls");
-            render();
-            break;
-        case 's':
-            // code for arrow down
-            move_down();
-            system("cls");
-            render();
-            break;
-        case 'd':
-            // code for arrow right
-            move_right();
-            system("cls");
-            render();
-            break;
-        case 'a':
-            // code for arrow left
-            move_left();
-            system("cls");
-           
-            render();
-            break;
-    }
-}
 
 int main(void){
     render();
---
+++ movement.h
@@ -0,0 +1,39 @@
+#ifndef MOVEMENT
+#define MOVEMENT
+
+#include "render.h"
+#include <stdlib.h>
+#include <conio.h>
+
+void movement_pacman(int key){
+    switch(key) { // the real value
+        case 'w':
+            // code for arrow up
+            move_up();
+            
+            system("cls");
+            render();
+            break;
+        case 's':
+            // code for arrow down
+            move_down();
+            system("cls");
+            render();
+            break;
+        case 'd':
+            // code for arrow right
+            move_right();
+            system("cls");
+            render();
+            break;
+        case 'a':
+            // code for arrow left
+            move_left();
+            system("cls");
+           
+            render();
+            break;
+    }
+}
+
+#endif
